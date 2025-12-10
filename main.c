e63c9eeb3d50764f07337c8649c5b62de5a0aa5d
Saadetatabay <etatabaysaad143@gmail.com>
2025-12-10 23:24:35 +0300

    comment

--- main.c
+++ main.c
@@ -10,14 +10,17 @@ int	main(int argc, char	*argv[], char *envp[])
 	//sonusuz shell döngüsü
 	while (1)
 	{
+		//readlien ekrana minishell$ yazını basar ve kullanıcıdan girdi bekler
+		//kullanıcı yazı yazarken yukarı aşağı ok backspace(silme) kullanabiliyor
+		// dönüş değeri kullanıcının girdiği string sonuna newline koymaz
 		input = readline("minishell$ ");
-		// CTRL-D EOF kontrolü yaptım
+		// CTRL-D EOF kontrolü yaptım readline NULL döner
 		if (!input)
 		{
 			printf("exit\n");
 			break;
 		}
-
+		//kullanıcı hiçbir şey girmeden ENTER'A basarsa boş string döner.boş satırı hsitorye gömmüyorum
 		//boş satır değilse historye ekliyroum
 		if (*input)
 		{
