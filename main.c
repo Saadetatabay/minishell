2743ccdd1aee2b543d44855c2f27e81799cbc09c
Saadetatabay <etatabaysaad143@gmail.com>
2025-12-10 22:32:42 +0300

    genel main yapısı oluşturuldu

---
+++ main.c
@@ -0,0 +1,32 @@
+int	main(int argc, char	*argv[], char *envp[])
+{
+	char	*input;
+	//unused uyarısı almamk için yaptım
+	(void)argc;
+	(void)argv;
+	(void)envp;
+
+	//sonusuz shell döngüsü
+	while (1)
+	{
+		input = readline("minishell$ ");
+		// CTRL-D EOF kontrolü yaptım
+		if (!input)
+		{
+			printf("exit\n");
+			break;
+		}
+
+		//boş satır değilse historye ekliyroum
+		if (*input)
+		{
+			add_history(input);
+			//saadet
+			parse_input(input);
+			//ayşe
+			//exec_command();
+		}
+		free(input);
+	}
+	return (0);
+}
