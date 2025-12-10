47accd6cf5b58e713915919b9dea44ccbe71f788
Saadetatabay <etatabaysaad143@gmail.com>
2025-12-10 23:59:07 +0300

    lexer function yapısı kuruldu

--- lexer.c
+++ lexer.c
@@ -1,2 +1,25 @@
 #include "minishell.h"
 
+t_token	*lexer(char	*input)
+{
+	int		i;
+	t_token	*token_list;
+
+	token_list = NULL;
+	i = 0;
+	while (input[i])
+	{
+		while (input[i] && (input[i]==' ' || input[i] == '\t'))
+			i++;
+		if (!input[i])
+			break;
+		//operatörleri yakalmak için
+		if (input[i] == '|' || input[i] == '<' || input[i] == '>')
+			i += handle_operator(&token_list, input, i);
+		else
+		{
+			//kelime kısmı
+		}
+	}
+	return (token_list);
+}
