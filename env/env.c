e5477fd423b9a451582acc2a2c8b4ffa1c581613
Saadetatabay <etatabaysaad143@gmail.com>
2025-12-13 21:36:43 +0300

    feat: implement function to add new environment nodes to the list

--- env/env.c
+++ env/env.c
@@ -12,3 +12,17 @@ t_env   *new_env_node(char *key, char *value)
 	new->next = NULL;
 	return (new);
 }
+
+void	env_add_back(t_env **env_list, t_env *new)
+{
+	t_env	*temp;
+	if (!*env_list)
+	{
+		*env_list = new;
+		return ;
+	}
+	temp = *env_list;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
+}
