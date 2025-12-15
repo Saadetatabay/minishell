22e1d7071a04c65676702713ba4a488b9b05d7be
Saadetatabay <etatabaysaad143@gmail.com>
2025-12-15 20:45:55 +0300

    feat: implement remove_quotes function to handle quote removal from strings

--- utils.c
+++ utils.c
@@ -8,3 +8,33 @@ void    print_env(t_env *env)
         env = env->next;
     }
 }
+
+char    *remove_quotes(char *str)
+{
+    char    *new;
+    int     i;
+    int     j;
+    int     single_quote;
+    int     double_quote;
+
+    if (!str)
+        return (NULL);
+    single_quote = 0;
+    double_quote = 0;
+    i = 0;
+    new = malloc(sizeof(char)*(ft_strlen(str) + 1));
+    while (str[i])
+    {
+        if (str[i] == '\'' && !double_quote)
+                single_quote = !single_quote;
+        else if (str[i] == '"' && !single_quote)
+                    double_quote = !double_quote;
+        else
+        {
+            new[j] = str[i];
+            j++;
+        }
+        i++;
+    }
+    return (new);
+}
