022ca7df4b2824d6076d4a07539776a456edc076
Saadetatabay <etatabaysaad143@gmail.com>
2025-12-15 22:38:23 +0300

    feat: add add_redirect function for handling command redirections

--- parser_utils.c
+++ parser_utils.c
@@ -41,3 +41,35 @@ char	**join_str_array(char *arr[], char *new_str)
 		free(arr);
 	return (new_array);
 }
+
+t_redir	*add_redirect(t_token *token, t_cmd *cmd)
+{
+	t_redir	*redir_new;
+	t_redir	*temp;
+
+	if (!token->next || token->next->value != WORD)
+		return NULL; //BURADA HATA GELECEK SONRA
+	redir_new = malloc(sizeof(t_redir));
+	if (!redir_new)
+		return NULL; // MALLOC HATASI
+	redir_new->file = ft_strdup(token->next->value);
+	redir_new->next = NULL;
+	if (token->type == REDIRECT_IN)
+		redir_new->type = REDIR_IN;
+	else if (token->type == REDIRECT_OUT)
+		redir_new->type = REDIR_OUT;
+	else if (token->type == APPEND)
+		redir_new->type = REDIR_APPEND;
+	else if (token->type == HEREDOC)
+		redir_new->type = REDIR_HEREDOC;
+	if (!cmd)
+		cmd->redirections = redir_new;
+	else
+	{
+		temp = cmd->redirections;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = redir_new;
+	}
+	return (redir_new);
+}
