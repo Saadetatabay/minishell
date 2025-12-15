7f5a9e6c1f28df1e36d64b24d4d45074b87a0b90
Saadetatabay <etatabaysaad143@gmail.com>
2025-12-15 22:07:26 +0300

    the funciton that joins str to array added

--- parser_utils.c
+++ parser_utils.c
@@ -13,3 +13,31 @@ t_cmd   *new_cmd_node(void)
     cmd->next = NULL;
     return (cmd);
 }
+
+char	**join_str_array(char *arr[], char *new_str)
+{
+    int		size;
+	int		j;
+    char    **new_array;
+
+    size = 0;
+    if(arr)
+    {
+        while (arr[size])
+            size++;
+    }
+	new_array = malloc(sizeof(char *)*(size + 2));
+	if (!new_array)
+		return NULL;
+	j = 0;
+	while (j < size)
+	{
+		new_array[j] = ft_strdup(arr[j]);
+		j++;
+	}
+	new_array[j] = ft_strdup(new_str);
+	new_array[j + 1] = NULL;
+	if (arr)
+		free(arr);
+	return (new_array);
+}
