2f5630d51b577f829a09ea25920e3b66683f9890
saadetatabay <etatabaysaad143@gmail.com>
2026-01-31 12:48:26 +0300

    normientte yapıldı

--- parser/parser_utils.c
+++ parser/parser_utils.c
@@ -1,38 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: satabay <satabay@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/31 12:16:31 by satabay           #+#    #+#             */
+/*   Updated: 2026/01/31 12:48:04 by satabay          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-t_cmd   *new_cmd_node(void)
+t_cmd	*new_cmd_node(void)
 {
-    t_cmd   *cmd;
+	t_cmd	*cmd;
 
-    cmd = malloc(sizeof(t_cmd));
-    if (!cmd)
-        return NULL;
-    //içindeki değerlerii NULL yapı-ıyrouz çünkü kontrol ettiğimizde çöp değer olmamalı
-    cmd->args = NULL;
-    cmd->redirections = NULL;
-    cmd->next = NULL;
-    return (cmd);
+	cmd = malloc(sizeof(t_cmd));
+	if (!cmd)
+		return (NULL);
+	cmd->args = NULL;
+	cmd->redirections = NULL;
+	cmd->next = NULL;
+	return (cmd);
 }
 
 char	**join_str_array(char *arr[], char *new_str)
 {
-    int		size;
+	int		size;
 	int		j;
-    char    **new_array;
+	char	**new_array;
 
-    size = 0;
-    if(arr)
-    {
-        while (arr[size])
-            size++;
-    }
-	new_array = malloc(sizeof(char *)*(size + 2));
+	size = 0;
+	while (arr && arr[size])
+		size++;
+	new_array = malloc(sizeof(char *) * (size + 2));
 	if (!new_array)
-		return NULL;
+		return (NULL);
 	j = 0;
 	while (j < size)
 	{
 		new_array[j] = ft_strdup(arr[j]);
+		if (arr)
+			free(arr[j]);
 		j++;
 	}
 	new_array[j] = ft_strdup(new_str);
@@ -48,28 +58,15 @@ t_redir	*add_redirect(t_token *token, t_cmd *cmd)
 	t_redir	*temp;
 
 	if (!token->next)
-	{
-		printf("Minishell: syntax error near unexpected token 'newline'\n");
-        return (NULL);
-	}
-	if(token->next->type != WORD)
-	{
-		printf("Minishell: syntax error near unexpected token '%s'\n", token->next->value);
-        return (NULL);
-	}
+		return (syntax_error("newline"));
+	if (token->next->type != WORD)
+		return (syntax_error(token->next->value));
 	redir_new = malloc(sizeof(t_redir));
 	if (!redir_new)
-		return NULL; // MALLOC HATASI
+		return (NULL);
 	redir_new->file = ft_strdup(token->next->value);
 	redir_new->next = NULL;
-	if (token->type == REDIRECT_IN)
-		redir_new->type = REDIR_IN;
-	else if (token->type == REDIRECT_OUT)
-		redir_new->type = REDIR_OUT;
-	else if (token->type == APPEND)
-		redir_new->type = REDIR_APPEND;
-	else if (token->type == HEREDOC)
-		redir_new->type = REDIR_HEREDOC;
+	redir_new->type = redir_type_atama(token->type);
 	if (!cmd->redirections)
 		cmd->redirections = redir_new;
 	else
@@ -80,4 +77,23 @@ t_redir	*add_redirect(t_token *token, t_cmd *cmd)
 		temp->next = redir_new;
 	}
 	return (redir_new);
+}
+
+void	*syntax_error(char *s)
+{
+	printf("Minishell: syntax error near unexpected token '%s'\n", s);
+	return (NULL);
+}
+
+int	redir_type_atama(int token_type)
+{
+	if (token_type == REDIRECT_IN)
+		return (REDIR_IN);
+	else if (token_type == REDIRECT_OUT)
+		return (REDIR_OUT);
+	else if (token_type == APPEND)
+		return (REDIR_APPEND);
+	else if (token_type == HEREDOC)
+		return (REDIR_HEREDOC);
+	return (0);
 }
