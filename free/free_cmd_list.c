96bd93428b07c4866f950f27e1ebcb610b134d51
Saadet Yuksel Atabay <satabay@k1m12s02.42istanbul.com.tr>
2026-02-14 12:34:36 +0300

    norminette free

--- free/free_cmd_list.c
+++ free/free_cmd_list.c
@@ -1,56 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_cmd_list.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: satabay <satabay@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/14 12:31:39 by satabay           #+#    #+#             */
+/*   Updated: 2026/02/14 12:33:11 by satabay          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-void    free_str_array(char **arr)
+void	free_str_array(char **arr)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    if (!arr)
-        return;
-    while (arr[i])
-    {
-        free(arr[i]);
-        i++;
-    }
-    free(arr);
+	i = 0;
+	if (!arr)
+		return ;
+	while (arr[i])
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
 }
 
-void    free_redirections(t_redir *redir)
+void	free_redirections(t_redir *redir)
 {
-    t_redir *tmp;
+	t_redir	*tmp;
 
-    while (redir)
-    {
-        tmp = redir->next;
-        if (redir->file)
-            free(redir->file);
-        free(redir);
-        redir = tmp;
-    }
+	while (redir)
+	{
+		tmp = redir->next;
+		if (redir->file)
+			free(redir->file);
+		free(redir);
+		redir = tmp;
+	}
 }
 
-void    free_cmd_list(t_cmd **cmd)
+void	free_cmd_list(t_cmd **cmd)
 {
-    t_cmd *tmp;
-    t_cmd *current;
+	t_cmd	*tmp;
+	t_cmd	*current;
 
-    if (!cmd || !*cmd)
-        return;
-    current = *cmd;
-    while (current)
-    {
-        tmp = current->next;
-        
-        //argümanları free
-        if (current->args)
-            free_str_array(current->args);
-            
-        //redir frere
-        if (current->redirections)
-            free_redirections(current->redirections);
-            
-        free(current);
-        current = tmp;
-    }
-    *cmd = NULL;
+	if (!cmd || !*cmd)
+		return ;
+	current = *cmd;
+	while (current)
+	{
+		tmp = current->next;
+		if (current->args)
+			free_str_array(current->args);
+		if (current->redirections)
+			free_redirections(current->redirections);
+		free(current);
+		current = tmp;
+	}
+	*cmd = NULL;
 }
--- free/free_token_list.c
+++ free/free_token_list.c
@@ -1,20 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_token_list.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: satabay <satabay@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/14 12:33:29 by satabay           #+#    #+#             */
+/*   Updated: 2026/02/14 12:34:20 by satabay          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-void    free_token_list(t_token **token)
+void	free_token_list(t_token **token)
 {
-    t_token *tmp;
-    t_token *current;
+	t_token	*tmp;
+	t_token	*current;
 
-    if (!token || !*token)
-        return;
-    current = *token;
-    while (current)
-    {
-        tmp = current->next;
-        if (current->value)
-            free(current->value); //string
-        free(current);            //düğüm
-        current = tmp;
-    }
-    *token = NULL;
+	if (!token || !*token)
+		return ;
+	current = *token;
+	while (current)
+	{
+		tmp = current->next;
+		if (current->value)
+			free(current->value);
+		free(current);
+		current = tmp;
+	}
+	*token = NULL;
 }
